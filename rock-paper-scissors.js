6b2e21c90b6316688815b8ff2a66fdd4cb749b52
Sebastian Kraska <140939975+sebastiankraska@users.noreply.github.com>
2025-03-16 13:05:45 +0100

    Add pseudo-based on exercise

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,20 @@
 console.log("Hello, World!")
+
+// function getComputerChoice
+    // must return rock, paper or scisscors
+    // uses Math.random - how to use this for 3 choices?
+
+// function getHumanChoice
+    // use prompt to get users input
+    // assume user always enters valid strings
+
+// Keep track of score with humanScore and computerScore
+
+// new function named playRound
+    // input: two paramters "humanChoice" (from getHumanChoice) and "computerChoice" (from getComputerChoice)
+    // write a message to console.log, e.g. "You lose, Paper beats Rock"
+    // increment humanScore and computerScore
+
+// new function named playGame
+    // should contain playRound and both score variables
+    // Play 5 rounds
