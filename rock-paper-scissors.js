9ea97dee50635b41d252e8d0ede5edce36689f10
Sebastian Kraska <140939975+sebastiankraska@users.noreply.github.com>
2025-03-17 08:56:54 +0100

    Add getComputerChoice and declare const for choices for User and Computer

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,8 @@
 console.log("Hello, World!")
 
+// declaring choices for Computer and User
+const choices = ["rock", "paper", "scissors"];
+
 // mapping game rules
 const gameRules = new Map([
     ["rock", "scissors"], // rock beats scissors
@@ -12,9 +15,26 @@ for (const [key, value] of gameRules.entries()) {
     console.log(`${key} beats ${value}`);
 }
 
+function getComputerChoice() {
+    const randomIndex = Math.floor(Math.random() * choices.length); // Math random creates random number between 0 and 1, length equals 3, example: Math.random creates 0.8112313213 * 3 = 2.4...., Math Floor = 2, index 2 means the third item in the array
+    return choices[randomIndex];
+}
+
+// testing random Computer Choice
+console.log(getComputerChoice()); // Outputs either "rock", "paper", or "scissors"
+console.log(getComputerChoice()); // Outputs either "rock", "paper", or "scissors"
+console.log(getComputerChoice()); // Outputs either "rock", "paper", or "scissors"
+console.log(getComputerChoice()); // Outputs either "rock", "paper", or "scissors"
+console.log(getComputerChoice()); // Outputs either "rock", "paper", or "scissors"
+console.log(getComputerChoice()); // Outputs either "rock", "paper", or "scissors"
+console.log(getComputerChoice()); // Outputs either "rock", "paper", or "scissors"
+console.log(getComputerChoice()); // Outputs either "rock", "paper", or "scissors"
+console.log(getComputerChoice()); // Outputs either "rock", "paper", or "scissors"
+
 // function getComputerChoice
     // must return rock, paper or scisscors
     // uses Math.random - how to use this for 3 choices?
+
 
 // function getHumanChoice
     // use prompt to get users input
