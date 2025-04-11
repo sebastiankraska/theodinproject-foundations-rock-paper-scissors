23576de9a73094f50b2aee833d8a9d732df9008e
Sebastian Kraska <140939975+sebastiankraska@users.noreply.github.com>
2025-04-11 07:07:52 +0200

    Redirect YouLose, Its-a-Tie and TotalScoreDisplay to div (logArea)

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -9,11 +9,15 @@ const scores = { human: 0, computer: 0, ties: 0, errors: 0 };
 function checkScores() {
     // play the game until one player reaches 5 points
     if (scores.computer >= 5 || scores.human >= 5) {
-        console.log("TOTAL SCORES:");
-        console.log("Human Score: " + scores.human);
-        console.log("Computer Score: " + scores.computer);
-        console.log("Ties: " + scores.ties);
+        // console.log("TOTAL SCORES:");
+        // console.log("Human Score: " + scores.human);
+        // console.log("Computer Score: " + scores.computer);
+        // console.log("Ties: " + scores.ties);
         console.log("Errors: " + scores.errors);
+        const text = document.createTextNode(
+            "TOTAL SCORES:" + "Human Score: " + scores.human + "Computer Score: " + scores.computer + "Ties: " + scores.ties,
+          );
+        logArea.appendChild(text);
     };
 };
 function getComputerChoice() {
@@ -26,17 +30,25 @@ function playRound() {
     const userchoice = event.target.name;
     const computerChoice = getComputerChoice();
     if (computerChoice == userchoice) {
-        console.log("IT'S A TIE! Both you and the computer chose " + userchoice + ".");
+        // console.log("IT'S A TIE! Both you and the computer chose " + userchoice + ".");
+        const text = document.createTextNode(
+            "IT'S A TIE! Both you and the computer chose " + userchoice + ".",
+          );
+        logArea.appendChild(text);
         scores.ties++;
     } else if (gameRules.get(userchoice) == computerChoice) {
-        console.log("YOU WIN! " + userchoice + " beats " + computerChoice + ".");
+        // console.log("YOU WIN! " + userchoice + " beats " + computerChoice + ".");
         const text = document.createTextNode(
             "YOU WIN! " + userchoice + " beats " + computerChoice + ".",
           );
         logArea.appendChild(text);
         scores.human++;
     } else if (gameRules.get(computerChoice) == userchoice) {
-        console.log("YOU LOSE! " + computerChoice + " beats " + userchoice + ".");
+        // console.log("YOU LOSE! " + computerChoice + " beats " + userchoice + ".");
+        const text = document.createTextNode(
+            "YOU LOSE! " + computerChoice + " beats " + userchoice + ".",
+          );
+        logArea.appendChild(text);
         scores.computer++;
     };
     checkScores();
